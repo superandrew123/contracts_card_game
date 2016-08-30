8b0e68ac31dd4ef34894ac2a53d5f7612974beb4
superandrew123 <andrewmiksch@gmail.com>
2016-08-30 19:19:53 -0400

    Needs real data

--- assets/game.js
+++ assets/game.js
@@ -1,2 +1,49 @@
 // game mechanics for Contracts
-console.log("made it");
+$(document).ready(function(){
+    var move = 0;
+    var cards = 7;
+    var lessCards = true;
+    var gameData = [
+        "First",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Clubs",
+        "Last",
+    ];
+    var advanceMove = function(){
+        if(move == 13){
+            $('.suit').html("Total");
+            $('.number-of-cards').html("Scores");
+            return;
+        }
+        $(".suit").html(gameData[move]);
+        var plural = cards == 1 ? " card" : " cards";
+        $(".number-of-cards").html(cards + plural);
+        if(lessCards){
+
+            $('.next').html('Next: ' + gameData[move + 1] + " " + (cards - 1));
+            cards--;
+        } else {
+            if(move == 12){
+                $('.next').html("Last Round");    
+            } else {
+                $('.next').html('Next: ' + gameData[move + 1] + " " + (cards + 1));
+            }
+            cards++;
+        }
+        if(cards == 1){
+            lessCards = false;
+        }
+        move ++;
+    }
+    advanceMove();
+    $("html").click(advanceMove);
+});
