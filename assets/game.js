6aba651a29a288e8e50b2b22a06fedd589a66f18
superandrew123 <andrewmiksch@gmail.com>
2016-08-30 21:28:15 -0400

    suit symbols

--- assets/game.js
+++ assets/game.js
@@ -4,38 +4,63 @@ $(document).ready(function(){
     var cards = 7;
     var lessCards = true;
     var gameData = [
-        "Hearts",
-        "Spades",
-        "Clubs",
-        "Diamonds",
+        "hearts",
+        "spades",
+        "clubs",
+        "diamonds",
         "None",
-        "Hearts",
-        "Spades",
-        "Clubs",
-        "Diamonds",
+        "hearts",
+        "spades",
+        "clubs",
+        "diamonds",
         "None",
-        "Hearts",
-        "Spades",
-        "Clubs",
+        "hearts",
+        "spades",
+        "clubs",
     ];
     var advanceMove = function(){
         if(move >= 13){
             $('.suit').html("Total");
             $('.number-of-cards').html("Scores");
             $('.next').html("");  
+            $(".suit").css("color", "black");
             return;
         }
         var plural = cards == 1 ? " card" : " cards";
-        $(".suit").html(gameData[move]);
+        var suit = gameData[move];
+        var html = 'None';
+        var color = 'black';
+        var nextText = '';
+        switch(suit) {
+            case "hearts":
+                html = "<span> &hearts; </span>";
+                color = "red";
+                break;
+            case "spades":
+                html = "<span> &spades; </span>";
+                break;
+            case "clubs":
+                html = "<span> &clubs; </span>";
+                color = "blue";
+                break;
+            case "diamonds":
+                html = "<span> &diams; </span>";
+                color = "green";
+                break;
+        }
+        $(".suit").html(html);
+        $(".suit").css("color", color);
         $(".number-of-cards").html(cards + plural);
         if(lessCards){
-            $('.next').html('Next: ' + gameData[move + 1] + " " + (cards - 1));
+            nextText = 'Next: ' + gameData[move + 1].charAt(0).toUpperCase() + gameData[move + 1].slice(1); + " " + (cards - 1);
+            $('.next').html(nextText);
             cards--;
         } else {
+            nextText = 'Next: ' + gameData[move + 1].charAt(0).toUpperCase() + gameData[move + 1].slice(1); + " " + (cards + 1)
             if(move == 12){
                 $('.next').html("Last Round");    
             } else {
-                $('.next').html('Next: ' + gameData[move + 1] + " " + (cards + 1));
+                $('.next').html(nextText);
             }
             cards++;
         }
